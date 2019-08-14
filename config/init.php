57f031c44a0e76d3783b950d1d9dac5372f5b882
Mars78 <sevlar78@gmail.com>
2019-08-14 18:15:11 +0300

    htacces init.php КОНСТАНТЫ

--- config/init.php
+++ config/init.php
@@ -1,2 +1,23 @@
 <?php
 
+define("DEBUG", 1);
+define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . '/public');
+define("APP", ROOT . '/app');
+define("CORE", ROOT . '/vendor/udemy/core');
+define("LIBS", ROOT . '/vendor/udemy/libs');
+define("CACHE", ROOT . '/tmp/cache');
+define("CONF", ROOT . '/config');
+define("LAYOUT", 'default');
+
+$app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}"; // http://udemy/public/index.php
+
+$app_path = preg_replace("#[^/]+$#", '', $app_path);
+
+$app_path = str_replace('/public/', '', $app_path);
+
+define("PATH", $app_path);
+
+define("ADMIN", PATH . '/admin');
+
+require_once ROOT . '/vendor/autoload.php';
--- public/index.php
+++ public/index.php
@@ -2,3 +2,6 @@
 
 
 require_once dirname(__DIR__) . '/config/init.php';
+
+
+echo "<h2 style='color: blue'>Тестовая строка из файла index.php</h2>";
